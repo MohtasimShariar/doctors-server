bd5f45886ccd2fd8a46fd7559624e6d03ce6f4e8
Md.Mohtasim Shariar <onim04@gmail.com>
2021-04-13 13:38:57 +0600

    all ok

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const MongoClient = require("mongodb").MongoClient;
 const port = process.env.PORT || 5055;
 
 
+
 app.use(cors());
 app.use(bodyParser.json());
  app.use(express.static('doctors'));
 app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0q8xi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
 
 
 const client = new MongoClient(uri, {
